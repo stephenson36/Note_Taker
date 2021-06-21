0f6460e50ce8566fe200a5ac2e3a08a1ffdc1331
Sophia Stephenson <stephenson3@wisc.edu>
2021-06-21 15:31:54 -0500

    fixed json issue

--- server.js
+++ server.js
@@ -10,8 +10,9 @@ const PORT = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
-require('./routes/htmlRoutes')(app);
 require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
 
 app.listen(PORT, () => {
     console.log(`App is listening on PORT: ${PORT}`);
